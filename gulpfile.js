b4eff4f45216daf45c99422b61d4edb9f1113c5b
Werner Michael <michael.ferwer@gmail.com>
2015-03-04 23:06:42 +0100

    add gulp, bower and browserify tools

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// gulp
+var gulp = require('gulp');
+
+// Import dependencies
+var browserify = require('browserify');
+var uglify =require('gulp-uglify');
+var source = require('vinyl-source-stream');
+var watchify = require('watchify');
+var gutil = require('gulp-util');
+
+// browserify files
+var browserify_file = browserify('./public/js/app.js',{debug: true});
+// watchify browserify files
+var watch = watchify(browserify_file);
+
+// build function that bundle js files in debug mode
+var build = function(option) {
+    return function() {
+        option
+            .bundle()
+            .pipe(source('app.js'))
+            .pipe(gulp.dest('./public/build/'))
+            .pipe(livereload());
+    };
+};
+
+// watch js files and rebuild them
+var watching = function(){
+    // watch
+    watch.on('update',build(watch));
+    // log when is update
+    watch.on('log',gutil.log);
+    // first build
+    build(watch)();
+}
+
+// build task
+gulp.task('build', build(browserify_file));
+// watch task
+gulp.task('watch',watching);
+
+
--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,21 @@
  * Created by michael on 09/04/2014.
  */
 
+//
+//require.config({
+//    baseUrl: "/"
+//});
+//
+////bootstrap of angular application
+//require(["js/angularjs/Application","js/angularjs/Router"],
+//    function(){
+//        angular.bootstrap(document.getElementsByTagName("html")[0],["Application"]);
+//    }
+//);
 
-
-require.config({
-    baseUrl: "/"
-});
-
-//bootstrap of angular application
-require(["js/angularjs/Application","js/angularjs/Router"],
-    function(){
-        angular.bootstrap(document.getElementsByTagName("html")[0],["Application"]);
-    }
-);
+// load angular
+var angular = require('angular');
+// load jquery
+var $ = jQuery = require('jquery');
+// load bootstrap
+require('bootstrap');
